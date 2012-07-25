27e5e785856af0b8890ce0794dfd0bc966e2ca85
Brandon Wamboldt <brandon.wamboldt@gmail.com>
2012-07-25 11:17:07 -0300

    Incremented the version number

--- util.php
+++ util.php
@@ -10,7 +10,7 @@
  *
  * @author   Brandon Wamboldt
  * @link     http://github.com/brandonwamboldt/utilphp/ Official Documentation
- * @version  1.0.001
+ * @version  1.0.002
  */
 
 declare(encoding='UTF-8');
